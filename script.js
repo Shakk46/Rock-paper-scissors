30615f5a65c54e03f514bcc5bf7782e099e6429c
Shakk46 <shaxrik46@gmail.com>
2022-08-02 23:09:22 +0500

    First version of rock-paper-scissors. A bit wakky (terrible). Just tried it in office. Done without any problems. Looking forward to modify the code as well as the interface (now it's just prompt and alert). BTW you need to write words in lower case. I'll add case-insensivity in next updates. CHEERS!!!.

--- script.js
+++ script.js
@@ -2,4 +2,43 @@ function getComputerChoice () {
     let arr = ['rock','paper','scissors']
     return arr[Math.floor(Math.random() * arr.length)]
 }
-console.log(getComputerChoice())
+
+let askPlayer = prompt('Whats your choice?')
+
+function roundRockPS (computerChoice, playerChoice) {
+    if (computerChoice == 'rock') {
+        if (playerChoice == 'paper') {
+            return 'rock. You won!!!'
+        }
+        if (playerChoice == 'rock') {
+            return "rock. It's a draw!!!"
+        }
+        if (playerChoice == 'scissors') {
+            return 'rock. You lost!!!'
+        }
+    }
+    else if (computerChoice == 'paper') {
+        if (playerChoice == 'scissors') {
+            return 'paper. You won!!!'
+        }
+        if (playerChoice == 'paper') {
+            return "paper.It's a draw!!!"
+        }
+        if (playerChoice == 'rock') {
+            return 'paper. You lost!!!'
+        }
+    }
+    else if (computerChoice == 'scissors') {
+        if (playerChoice == 'rock') {
+            return 'scissors. You won!!!'
+        }
+        if (playerChoice == 'scissors') {
+            return "scissors. It's a draw!!!"
+        }
+        if (playerChoice == 'paper') {
+            return 'scissors.You lost!!!'
+        }
+    }
+}
+
+alert(roundRockPS(getComputerChoice(),askPlayer));
